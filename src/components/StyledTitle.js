fc21743f1d4003c96de66b06edee45dd336d391e
thiago-arend <thiago.arend@gmail.com>
2023-07-10 02:58:42 -0300

    save final

--- src/components/StyledTitle.js
+++ src/components/StyledTitle.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const StyledTitle = styled.h1`
-    color: #FFF;
+    color: blue;
     font-size: 32px;
     font-family: 'Saira Stencil One', cursive;
     margin-bottom: 38px;
