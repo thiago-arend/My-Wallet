d72a8d9cb3aae2b6233f9a639749747a9b7bf8ce
thiago-arend <thiago.arend@gmail.com>
2023-07-10 02:56:53 -0300

    save final

--- src/components/Saldo/styled.js
+++ src/components/Saldo/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SaldoContainer = styled.div`
     position: fixed;
     z-index: 2;
-    bottom: calc(100vh - 400px);
+    top: 560px;
     height: 25px;
     width: 306px;
     background-color: #FFF;
