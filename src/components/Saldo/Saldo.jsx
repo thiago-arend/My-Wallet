5fc550b5b57d09001252b19755e07ef2ea558c94
thiago-arend <thiago.arend@gmail.com>
2023-07-10 00:49:27 -0300

    correções

--- src/components/Saldo/Saldo.jsx
+++ src/components/Saldo/Saldo.jsx
@@ -1,10 +1,12 @@
 import { SaldoContainer } from "./styled";
 
 export default function Saldo({ saldo }) {
+    const saldoConverted = Math.abs(saldo.toFixed(2)).toString().replace(".", ",");
+
     return (
         <SaldoContainer saldo={saldo}>
             <span>SALDO</span>
-            <span data-test="total-amount">{Math.abs(saldo.toFixed(2))}</span>
+            <span data-test="total-amount">{saldoConverted}</span>
         </SaldoContainer>
     );
 }
